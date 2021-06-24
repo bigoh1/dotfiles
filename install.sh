578a85d89b869ff9180e0c3b9105b2e82c13cadd
multifrench <multifrench@protonmail.com>
2021-06-24 15:00:54 +0300

    Verify git is installed

--- install.sh
+++ install.sh
@@ -1,7 +1,31 @@
+isInstalled()
+{
+    if ! [ -x "$(command -v $1)" ]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
+isGitInstalled=$(isInstalled git)
+if [ $isGitInstalled -eq 0 ]; then
+    echo "Error: git is not installed..."
+    exit 1
+fi
+
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 echo '.dotfiles' >> $HOME/.gitignore
 
 git clone --bare https://github.com/multifrench/dotfiles.git $HOME/.dotfiles
 
-config checkout
-config config --local status.showUntrackedFiles no
+declare -a requiredSoftware=("zsh" "nvim" "exa")
+for i in "${requiredSoftware[@]}"; do
+    result=$(isInstalled "$i")
+    if [ $result -eq 0 ]; then
+        echo "Please, install $i"
+    fi
+done
+
+
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
